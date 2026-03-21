98062cdf35c33b1894712b9a10eb60b30291c919
Ashutosh Joshi <ashutoshjosheee@gmail.com>
2026-03-21 16:03:01 +0530

    - modified UI design

--- UiResumo/src/app/resume-template/resume-template.component.ts
+++ UiResumo/src/app/resume-template/resume-template.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderSectionComponent } from '../header-section/header-section.component';
+import { ExperienceSectionComponent } from '../experience-section/experience-section.component';
+import { EducationSectionComponent } from '../education-section/education-section.component';
+import { SkillsSectionComponent } from '../skills-section/skills-section.component';
 
 @Component({
   selector: 'app-resume-template',
-  imports: [],
+  imports: [HeaderSectionComponent,ExperienceSectionComponent,EducationSectionComponent,SkillsSectionComponent],
   templateUrl: './resume-template.component.html',
   styleUrl: './resume-template.component.scss'
 })
